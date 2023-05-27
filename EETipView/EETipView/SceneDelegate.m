666bef1b34120bb666226ef02212fbdf01e76b34
021159 <liufushan@htsc.com>
2023-05-27 17:21:33 +0800

    test11

--- EETipView/EETipView/SceneDelegate.m
+++ EETipView/EETipView/SceneDelegate.m
@@ -12,6 +12,7 @@
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     
+    
 }
 
 
